04e973a258ebdda6b8edef61b6419d1b988c60e9
Jellyman305 <robpetrie@Roberts-iMac.local>
2014-10-26 12:26:58 +0000

    Floor + basic gravity.
    The gravity needs work and the floor is simply a placeholder.

--- core/src/com/rob/katkiller/KatKiller.java
+++ core/src/com/rob/katkiller/KatKiller.java
@@ -16,63 +16,110 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class KatKiller extends ApplicationAdapter {
+	
 	SpriteBatch batch;
+	
+	//Textures
 	Texture blood;
 	Texture katImage;
+	Texture floorImage;
+	
+	//Camera
 	OrthographicCamera camera;
+	
+	//Shapes
 	Rectangle kat;
+	Rectangle floor;
+	
+	//Arrays
 	Array<Rectangle> bloodDrops;
 	long lastDropTime;
 	
 	@Override
 	public void create () {
+		
 		batch = new SpriteBatch();
+		
+		//Textures
 		blood = new Texture(Gdx.files.internal("blood.png"));
 		katImage = new Texture(Gdx.files.internal("kat.png"));
+		floorImage = new Texture(Gdx.files.internal("floor.png"));
+		
+		//Camera
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
+		
+		//Shapes
 		kat = new Rectangle();
 		kat.x = 800 / 2 - 22 / 2;
-		kat.y = 20;
+		kat.y = 100;
 		kat.width = 22;
 		kat.height = 40;
+		
+		floor = new Rectangle();
+		floor.x = 0;
+		floor.y = 0;
+		floor.width = 800;
+		floor.height = 20;
+		
+		//Start to spawn the blood
 		bloodDrops = new Array<Rectangle>();
 		spawnBloodDrop();
 		
 	}
 	
 	public void spawnBloodDrop() {
+		
 		Rectangle blooddrop = new Rectangle();
+		
 		blooddrop.x = MathUtils.random(0, 800-16);
 		blooddrop.y = 480;
+		
 		blooddrop.width = 16;
 		blooddrop.height = 16;
+		
 		bloodDrops.add(blooddrop);
 		lastDropTime = TimeUtils.nanoTime();
 	}
 
 	@Override
 	public void render () {
+		
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		camera.update();
+		//Batch
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+		
 		batch.draw(katImage, kat.x, kat.y);
+		
 		for(Rectangle blooddrop: bloodDrops) {
 			batch.draw(blood, blooddrop.x, blooddrop.y);
 		}
+		
+		batch.draw(floorImage, floor.x, floor.y);
+		
 		batch.end();
 		
 		if(Gdx.input.isTouched()) {
+			
 			Vector3 touchPos = new Vector3();
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
 			kat.x = touchPos.x - 22 / 2;
+			
 		}
+		
+		kat.y -= 1;
 		
 		if(kat.x < 0) kat.x = 0;
 		if(kat.x > 800 - 22) kat.x = 800 - 22;
+		
+		if(kat.overlaps(floor)) {
+			kat.y += 1;
+		}
 		
 		
 		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnBloodDrop();
